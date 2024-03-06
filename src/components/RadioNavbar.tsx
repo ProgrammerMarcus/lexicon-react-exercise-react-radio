5a00b295b8cda5098f8548f87ef9d0534de2ec7e
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-03-06 15:43:17 +0100

    improved navbar with better mobile support

--- src/components/RadioNavbar.tsx
+++ src/components/RadioNavbar.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 export function RadioNavbar() {
     const [value, setValue] = useState<string>("");
+    const [toggle, setToggle] = useState<boolean>(false);
     const navigate = useNavigate();
     function search(e: React.KeyboardEvent<HTMLInputElement>) {
         if (e.code === "Enter") {
@@ -13,19 +14,37 @@ export function RadioNavbar() {
     return (
         <>
             <nav className="RadioNavbar">
-                <h1 className="logo">
-                    <img src="/logo.svg" alt="RADIO" />
-                </h1>
-                <Link to="/" className="link">
-                    CHANNELS
-                </Link>
-                <Link to="/categories" className="link">
-                    CATEGORIES
-                </Link>
-                <Link to="/favorites" className="link">
-                    FAVORITES
-                </Link>
-                <input onKeyUp={search} onChange={(e) => setValue(e.target.value)} value={value} className="search" type="text" name="navbar-search" id="RadioNavbarSearch" placeholder="Search..." />
+                {!toggle && (
+                    <>
+                        <h1 className="logo">
+                            <img src="/logo.svg" alt="RADIO" />
+                        </h1>
+                        <Link to="/" className="link">
+                            CHANNELS
+                        </Link>
+                        <Link to="/categories" className="link">
+                            CATEGORIES
+                        </Link>
+                        <Link to="/favorites" className="link">
+                            FAVORITES
+                        </Link>
+                    </>
+                )}
+
+                {toggle && (
+                    <input
+                        autoFocus
+                        onKeyUp={search}
+                        onChange={(e) => setValue(e.target.value)}
+                        value={value}
+                        className="search"
+                        type="text"
+                        name="navbar-search"
+                        id="RadioNavbarSearch"
+                        placeholder="Search..."
+                    />
+                )}
+                <img onClick={() => setToggle(!toggle)} src={!toggle ? "/search.svg" : "/close.svg"} alt="Search icon" className="search-icon" />
             </nav>
         </>
     );
